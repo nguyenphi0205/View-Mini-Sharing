950994a59fa40f11068f827e84ffe6df4894420d
taiphan1411 <phanquangtai1996@gmail.com>
2018-04-27 21:48:31 +0700

    new file

---
+++ View/js/login.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    // Get the modal
+    var modal_log = document.getElementById('login_form');
+    var modal_reg = document.getElementById('register_form');
+
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == modal_log || event.target == modal_reg) {
+            modal_log.style.display = "none";
+            modal_reg.style.display = "none";
+        }
+    }
+});
