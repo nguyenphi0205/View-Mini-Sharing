9feb1f5836e5c5422d3b465cd7894ca49c0ce044
taiphan1411 <38754135+taiphan1411@users.noreply.github.com>
2018-05-03 16:52:53 +0700

    new file

--- View/sass/js/sreach.js
+++ View/sass/js/sreach.js
@@ -1,8 +1,78 @@
-$(document).ready(function(){
-    $("#search").on("keyup", function() {
-      var value = $(this).val().toLowerCase();
-      $("#view_file tr").filter(function() {
-        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
-      });
-    });
-  });
+var app = angular.module('myApp', []);
+app.controller('myCtrl', function ($scope) {
+
+  $scope.table = [{
+      category: "all",
+      items: [{
+        filename: "index.html",
+        category: "Code",
+        size: "10mb",
+        dateupload: "27/04/2018",
+        uploadby: "Phi đẹp trai",
+        download: "69"
+      }, {
+        filename: "bóng rổ",
+        category: "Sport",
+        size: "15mb",
+        dateupload: "27/04/2018",
+        uploadby: "Phi đẹp trai",
+        download: "69"
+      }, {
+        filename: "Đá bóng",
+        category: "Sport",
+        size: "10mb",
+        dateupload: "27/04/2018",
+        uploadby: "Phi đẹp trai",
+        download: "69"
+      }, {
+        filename: "Tin Tức",
+        category: "News",
+        size: "15mb",
+        dateupload: "27/04/2018",
+        uploadby: "Phi đẹp trai",
+        download: "69"
+      }]
+    },
+    {
+      category: "Sport",
+      items: [{
+        filename: "bóng rổ",
+        category: "Sport",
+        size: "15mb",
+        dateupload: "27/04/2018",
+        uploadby: "Phi đẹp trai",
+        download: "69"
+      }, {
+        filename: "Đá bóng",
+        category: "Sport",
+        size: "10mb",
+        dateupload: "27/04/2018",
+        uploadby: "Phi đẹp trai",
+        download: "69"
+      }]
+    },
+    {
+      category: "Code",
+      items: [{
+        filename: "index.html",
+        category: "Code",
+        size: "10mb",
+        dateupload: "27/04/2018",
+        uploadby: "Phi đẹp trai",
+        download: "69"
+      }]
+    },
+    {
+      category: "news",
+      items: [{
+        filename: "Tin Tức",
+        category: "News",
+        size: "10mb",
+        dateupload: "27/04/2018",
+        uploadby: "Phi đẹp trai",
+        download: "69"
+      }]
+    },
+  ];
+  $scope.select_search = $scope.table[0];
+});
